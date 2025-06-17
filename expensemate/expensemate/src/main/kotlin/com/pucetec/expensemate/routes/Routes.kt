0bcf3854da90d4138ad29fc2d7a18ec5d5823493
Alexander <a.alexanderpavon@gmail.com>
2025-06-16 21:50:16 -0500

    Crear la carpeta routes

---
+++ expensemate/expensemate/src/main/kotlin/com/pucetec/expensemate/routes/Routes.kt
@@ -0,0 +1,20 @@
+package com.pucetec.expensemate.routes
+
+object Routes {
+    const val BASE_URL = "/api/expensemate"
+
+    // USERS
+    const val USERS = "$BASE_URL/users"
+
+    // CATEGORIES
+    const val CATEGORIES = "$BASE_URL/categories"
+
+    // CREDIT CARDS
+    const val CREDIT_CARDS = "$BASE_URL/credit-cards"
+
+    // ACCOUNTS
+    const val ACCOUNTS = "$BASE_URL/accounts"
+
+    // MOVEMENTS
+    const val MOVEMENTS = "$BASE_URL/movements"
+}
